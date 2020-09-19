7c4e6b9cac214c50de47174c5e8db6a7fca5b0d7
cornfeedhobo <cornfeedhobo@fuzzlabs.org>
2020-09-19 16:58:56 -0500

    increase coverage on TestSetOutput

--- flag_test.go
+++ flag_test.go
@@ -859,9 +859,14 @@ func TestSetOutput(t *testing.T) {
 	flags.SetOutput(&buf)
 	flags.Init("test", ContinueOnError)
 	flags.Parse([]string{"--unknown"})
-	if out := buf.String(); !strings.Contains(out, "--unknown") {
-		t.Errorf("expected output mentioning unknown; got %q", out)
+	out := buf.String()
+	if out == "" {
+		t.Error("expected output, got none")
 	}
+	if strings.Contains(out, "--unknown") {
+		return
+	}
+	t.Errorf("expected output mentioning unknown; got %q", out)
 }
 
 func TestOutput(t *testing.T) {
